89ec747a25cc8c018e475a01eeaac6c83691e3c7
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2023-07-03 17:04:40 +0200

    New catalog build

---
+++ update-builder.sh
@@ -0,0 +1,4 @@
+cd open-science-catalog-builder
+git pull
+cd ..
+git commit open-science-catalog-builder -m "Updating open-science-catalog-builder"
---
+++ update-client.sh
@@ -0,0 +1,17 @@
+# cd stac-browser
+# npm install
+# npm run build -- --catalogUrl="https://eoepca.github.io/open-science-catalog-metadata-staging/catalog.json" --pathPrefix="/open-science-catalog-metadata-staging"
+
+
+
+
+
+# rm -rf ../client/*
+# cp -r dist/* ../client/
+# cd ..
+
+
+cd stac-browser
+npm install
+npm run build -- --catalogUrl="https://constantinius.github.io/osc-demo/collection.json" --pathPrefix="/osc-demo"
+
